8321020bde0075b8bfc162656bfe94a5d7013002
Allen Lin <eggs8021729@gmail.com>
2019-08-11 21:07:53 +0800

    add check file

--- products.py
+++ products.py
@@ -1,13 +1,20 @@
+import os # operating system
+
 #讀取檔案
-products = []                  #encoding='utf-8' 寫入讀取都要用一樣的編碼
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #跳過 跳到下一迴圈
-		name, price = line.strip().split(',') #spilt=切割 遇到逗點做分割
+products = [] #產生空清單 叫做 products
+
+if os.path.isfile('products.csv'):  #檢查檔案  #os=作業系統 path路徑 os.path.isfile 檢查檔案在嘛
+	print('yeah! 找到檔案了!')   
+	with open('products.csv', 'r', encoding='utf-8') as f: #encoding='utf-8' 寫入讀取都要用一樣的編碼
+		for line in f:
+			if '商品,價格' in line:
+				continue #跳過 跳到下一迴圈
+			name, price = line.strip().split(',') #spilt=切割 遇到逗點做分割
 			#.strip=去掉換行符號(\n) 
-		products.append([name, price])
-print(products)
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案.....')
 
 #讓使用者輸入
 while True:
@@ -24,6 +31,7 @@ for p in products:
 	print(p[0], '的價格是', p[1])
 
  
+
 #寫入檔案
                        #r=讀取 w=寫入   as f = 當作f = products.txt = f 只是簡稱
 with open('products.csv', 'w', encoding='utf-8') as f:   
